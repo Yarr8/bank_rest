233fe6b1d6ff057273519779c29b79ce169f484b
Arsenii <ars3577@gmail.com>
2025-09-21 21:44:26 +0300

    feat: add repositories for core entities

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,21 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByUserId(Long userId);
+    List<Card> findByUserIdAndStatus(Long userId, Card.CardStatus status);
+    Optional<Card> findByCardNumber(String cardNumber);
+
+    // pageable
+    Page<Card> findByUserId(Long userId, Pageable pageable);
+    Page<Card> findByUserIdAndStatus(Long userId, Card.CardStatus status, Pageable pageable);
+}
--- src/main/java/com/example/bankcards/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/bankcards/security/UserDetailsServiceImpl.java
@@ -1,22 +1,26 @@
 package com.example.bankcards.security;
 
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.entity.User;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.example.bankcards.entity.User;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO: get user from repository
-        // User user = ;
-        // return new JwtUser(user);
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new JwtUser(user);
     }
 }
